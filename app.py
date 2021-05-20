2449b1748c08382e3ed8a5036db57e87103e0825
Edward Kozlowski <ekozlowski1@gmail.com>
2021-05-19 23:11:05 -0500

    sysinfo

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
@@ -6,3 +7,6 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get('/sysinfo')
+async def sysinfo():
+    return os.system('cat /proc/cpuinfo')
